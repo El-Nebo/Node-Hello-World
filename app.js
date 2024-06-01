0539fc391e6c342d433b554fea24036d5a7e419b
Ghareeb <mohamed.ghareeb.ext@siemens.com>
2024-06-02 02:32:56 +0300

    1/5

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 3000
 
 
 app.use((req, res, next) => {
-    res.status(200).send('<h1>Hello World from Abbas!</h1>');
+    res.status(200).send(`
+    <h1>Hello World from Abbas!</h1>
+    <h1>يا رب فك الضيقه</h1>
+    <h1>1/3/2025 يا رب </h1>
+    `);
 });
 
 app.listen(PORT);
